2aec92a8fd866dd53c30eb26eb20f8583f13684b
juho H <juhoth@ovi.com>
2013-12-19 19:07:56 +0200

    initial commit

---
+++ src/main/java/vastus/sokoban/logic/Element.java
@@ -0,0 +1,10 @@
+package vastus.sokoban.logic;
+
+/**
+ * Abstract class describing elements in game.
+ * Element can be a player, box, storage or wall.
+ * 
+ * @author Juho Hautala
+ */
+public abstract class Element {
+}
---
+++ src/test/java/vastus/sokoban/logic/ElementTest.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vastus.sokoban.logic;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Test class for Element.
+ *
+ * @author juhoh
+ */
+public class ElementTest {
+    
+    @Before
+    public void setUp() {}
+
+}
