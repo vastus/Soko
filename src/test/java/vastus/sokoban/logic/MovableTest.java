48fa7387677c9831f66b77811c97dddfe38210e5
juho H <juhoth@ovi.com>
2013-12-19 21:00:50 +0200

    player type

---
+++ src/test/java/vastus/sokoban/logic/MovableTest.java
@@ -0,0 +1,17 @@
+package vastus.sokoban.logic;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Test class for movable.
+ *
+ * @author Juho Hautala
+ */
+public class MovableTest {
+
+    @Before
+    public void setUp() {}
+
+}
