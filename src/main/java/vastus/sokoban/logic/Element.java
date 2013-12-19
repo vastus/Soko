6df8cc54ad51f702d45636b4c52ca78cdad768c1
juho H <juhoth@ovi.com>
2013-12-19 20:19:15 +0200

    static types for elems and getters setter

--- src/main/java/vastus/sokoban/logic/Element.java
+++ src/main/java/vastus/sokoban/logic/Element.java
@@ -7,4 +7,78 @@ package vastus.sokoban.logic;
  * @author Juho Hautala
  */
 public abstract class Element {
+
+    /**
+     * Character type of player.
+     */
+    public static final char PLAYER = '@';
+
+    /**
+     * Character type of box.
+     */
+    public static final char BOX = 'O';
+
+    /**
+     * Character type of storage.
+     */
+    public static final char STORAGE = '+';
+
+    /**
+     * Character type of wall.
+     */
+    public static final char WALL = '#';
+
+    /**
+     * Character type of floor.
+     */
+    public static final char FLOOR = '.';
+
+    /**
+     * Type of element.
+     */
+    private final char type;
+
+    /**
+     * Position of element.
+     */
+    private Point position;
+
+    /**
+     * Constructor for elements.
+     * 
+     * @param type type of element
+     * @param position position of element
+     */
+    public Element(char type, Point position) {
+        this.type = type;
+        this.position = position;
+    }
+
+    /**
+     * Getter for type.
+     * 
+     * @return type of element
+     */
+    public char getType() {
+        return type;
+    }
+
+    /**
+     * Getter for position.
+     * 
+     * @return position of element
+     */
+    public Point getPosition() {
+        return position;
+    }
+
+    /**
+     * Setter for position.
+     * 
+     * @param position new position for element
+     */
+    public void setPosition(Point position) {
+        this.position = position;
+    }
+
 }
--- src/test/java/vastus/sokoban/logic/ElementTest.java
+++ src/test/java/vastus/sokoban/logic/ElementTest.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package vastus.sokoban.logic;
 
 import org.junit.Before;
@@ -17,5 +13,33 @@ public class ElementTest {
     
     @Before
     public void setUp() {}
+
+    @Test
+    public void shouldKnowCharsForElements() {
+        assertEquals('@', Element.PLAYER);
+        assertEquals('O', Element.BOX);
+        assertEquals('+', Element.STORAGE);
+        assertEquals('#', Element.WALL);
+        assertEquals('.', Element.FLOOR);
+    }
+
+    @Test
+    public void shouldGetType() {
+        assertEquals('#', new Wall(new Point(0, 0)).getType());
+    }
+
+    @Test
+    public void shouldGetPosition() {
+        Element wall = new Wall(new Point(-12, 289));
+        assertEquals(new Point(-12, 289), wall.getPosition());
+    }
+
+    @Test
+    public void shouldSetPosition() {
+        Point expected = new Point(43, 2);
+        Element wall = new Wall(new Point(0, 0));
+        wall.setPosition(new Point(43, 2));
+        assertEquals(expected, wall.getPosition());
+    }
 
 }
