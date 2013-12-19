7559af7d94cb97ed106bcf06e776c33531ce1e4e
juho H <juhoth@ovi.com>
2013-12-19 23:06:23 +0200

    javadoc

--- src/main/java/vastus/sokoban/logic/Box.java
+++ src/main/java/vastus/sokoban/logic/Box.java
@@ -1,7 +1,17 @@
 package vastus.sokoban.logic;
 
+/**
+ * Class describing movable box element.
+ * 
+ * @author Juho Hautala
+ */
 public class Box extends Movable {
 
+    /**
+     * Constructor for box with 
+     * 
+     * @param position initial position of box
+     */
     public Box(Point position) {
         super(Element.BOX, position);
     }
