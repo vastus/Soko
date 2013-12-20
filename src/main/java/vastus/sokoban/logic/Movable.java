b844f299eb42c73d28e1098fd7a77a99d9743c75
juho H <juhoth@ovi.com>
2013-12-20 18:32:32 +0200

    const javadoc

--- src/main/java/vastus/sokoban/logic/Movable.java
+++ src/main/java/vastus/sokoban/logic/Movable.java
@@ -3,11 +3,17 @@ package vastus.sokoban.logic;
 /**
  * Abstract class describing movable elements in game.
  * Movable can be a player or box.
- * 
+ *
  * @author Juho Hautala
  */
 public abstract class Movable extends Element {
 
+    /**
+     * Constructor for movable.
+     *
+     * @param type type of element
+     * @param position  initial position of element
+     */
     public Movable(char type, Point position) {
         super(type, position);
     }
